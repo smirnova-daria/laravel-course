34dcb73ca2dfb9bd1b9f5e943869b7a93368aaec
Daria Smirnova <darya.smirn1@yandex.ru>
2023-01-04 09:52:00 +0300

    init laravel project

---
+++ resources/views/contacts.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="/">Главная</a></li>
+				<li><a href="/about">О нас</a></li>
+				<li><a href="/contacts">Контакты</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<h1>Наши контакты</h1>
+	</main>
+</body>
+
+</html>
